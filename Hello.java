640198b98ffd32178f25368487562a5195405267
GokulVel <gokulsmart449@gmail.com>
2018-05-28 23:24:52 -0700

    byte conversion

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+public static void main(String s[]){
+System.out.println("welcome to java Training!!!");
+byte one = 100;
+byte two = 20;
+int three = one + two;
+system.out.println(three);
+}
+}
